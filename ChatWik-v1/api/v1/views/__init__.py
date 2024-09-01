89f315f9ccc1912b74d8d69afbf9c92194bacdcd
wikki2000 <wisdomokposin@gmail.com>
2024-09-01 20:26:36 +0100

    Restructurer Repo

--- ChatWik-v1/api/v1/views/__init__.py
+++ ChatWik-v1/api/v1/views/__init__.py
@@ -7,4 +7,6 @@ app_views = Blueprint('app_views', __name__)
 from api.v1.views.auth.login import *
 from api.v1.views.auth.send_token import *
 from api.v1.views.auth.register import *
+from api.v1.views.messages import *
+from api.v1.views.users import *
 from api.v1.views.index import *
---
+++ ChatWik-v1/api/v1/views/users.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Handle API views for message.""" 
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flasgger.utils import swag_from
+from models import storage
+from models.user import User
+
+
+@app_views.route("/all-users", strict_slashes=False)
+@swag_from("documentation/users/active_users.yml")
+def all_users():
+    """Retrieve all users."""
+    users = storage.all(User).values()
+    user_list = [
+            {
+                "id": user.id,
+                "username": user.name,
+                "email": user.email
+            } for user in users
+    ]
+    return jsonify(user_list)
+
+
+@app_views.route("/active-users", strict_slashes=False)
+@swag_from("documentation/users/all_users.yml")
+def active_users():
+    """Retrieve only active users."""
+    users = storage.all(User).values()
+
+    # Use list comprehension to filter active user
+    active_users = [user for user in users if user.is_active]
+    user_list = [
+            {
+                "id": user.id,
+                "username": user.name,
+                "email": user.email
+            } for user in active_users
+    ]
+    return jsonify(user_list)
